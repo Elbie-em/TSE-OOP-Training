0b82810f017236f0760aee113f93572bcc1dc014
Elbie-em <emoonga1993@gmail.com>
2021-10-14 15:40:33 +0200

    Create classroom class, build associations and add functionality

--- example_school_library_decorator/student.rb
+++ example_school_library_decorator/student.rb
@@ -1,9 +1,13 @@
 require './person.rb'
 
 class Student < Person
+  attr_writer :classroom
   def initialize(age, classroom, name = "Unknown", parent_permission = true)
     super(age, name, parent_permission)
     @classroom = classroom
+    if (!classroom.students.include?(self))
+      classroom.students.push(self)
+    end
   end
 
   def play_hooky
